bfcbeddfee4edc3db0c7e8b95d1982f1e7d43b1e
Yoann Reyne <yoann.reyne@etu.umontpellier.fr>
2023-09-26 08:39:36 +0200

    add

---
+++ Yoann/tp1.ast/src/main/java/org/example/visiteur/LineCountVisitor.java
@@ -0,0 +1,25 @@
+package org.example.visiteur;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+public class LineCountVisitor extends ASTVisitor {
+    private int lineCount = 0;
+
+    @Override
+    public boolean visit(MethodDeclaration node) {
+        // Obtenir le numéro de ligne de début et de fin du nœud
+        int startLine = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition());
+        int endLine = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition() + node.getLength());
+
+        // Ajouter le nombre de lignes du nœud
+        lineCount += endLine - startLine + 1;
+
+        return super.visit(node);
+    }
+
+    public int getLineCount() {
+        return lineCount;
+    }
+}
---
+++ Yoann/tp1.ast/src/main/java/org/example/visiteur/PackageDeclarationVisitor.java
@@ -0,0 +1,19 @@
+package org.example.visiteur;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.*;
+
+public class PackageDeclarationVisitor extends ASTVisitor {
+    private int packageCount = 0;
+
+    @Override
+    public boolean visit(PackageDeclaration node) {
+        // Compter chaque déclaration de package
+        packageCount++;
+        return super.visit(node);
+    }
+
+    public int getPackageCount() {
+
+        return packageCount;
+    }
+}
