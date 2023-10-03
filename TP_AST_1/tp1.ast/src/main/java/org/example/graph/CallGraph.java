f4b481eb0c573278363cc77e44ef447a23f6ccba
MN-Sandratra <mn.sandratra@gmail.com>
2023-10-03 12:00:08 +0200

    clean project

---
+++ TP_AST_1/tp1.ast/src/main/java/org/example/graph/CallGraph.java
@@ -0,0 +1,91 @@
+package org.example.graph;
+
+import org.eclipse.jdt.core.dom.*;
+import org.example.parser.Parser;
+import org.example.visiteur.ClassCountVisitor;
+import org.example.visiteur.MethodDeclarationVisitor;
+import org.example.visiteur.MethodInvocationVisitor;
+
+import java.util.*;
+
+public class CallGraph {
+
+    private Parser parser;
+
+    private Map<String, Map<String, List<MethodInvocation>>> graph = new HashMap<>();
+
+    private Set<Pair<String, String>> aretes;
+
+    public CallGraph(String projectPath){
+        this.parser = new Parser(projectPath);
+        this.aretes = new HashSet<>();
+    }
+
+    // Parcours d'un fichier java
+    public Map<String, Map<String, List<MethodInvocation>>> parcours(CompilationUnit cUnit){
+
+        ClassCountVisitor classVisitor = new ClassCountVisitor();
+
+        Map<String, List<MethodInvocation>> callMethods = new HashMap<>();
+
+        TypeDeclaration cls = classVisitor.getClasse(cUnit);
+
+        MethodDeclarationVisitor methodVisitor = new MethodDeclarationVisitor();
+
+        // Pour chaque méthode de la classe
+        for(MethodDeclaration method: methodVisitor.getMethods(cls)){
+
+            // Récupération de la liste des appels de méthode présent dans la méthode "method"
+            List<MethodInvocation> invocationMethods = getMethodInvocation(method);
+
+            invocationMethods.stream()
+                    .forEach(methodInvocation -> aretes.add(new Pair<>(method.getName().toString(),methodInvocation.getName().toString())));
+            // Ajout dans le hashmap => NomMethode : [ methodeAppelee1, methodeAppelee2 ..]
+            callMethods.put(method.getName().toString(), invocationMethods);
+
+        }
+
+        // Ajout dans le graph => Classe : hashmap
+        graph.put(String.valueOf(cls.getName()),callMethods);
+
+        return graph;
+    }
+
+
+    public boolean start(){
+
+        // Parcours du dossier
+        parser.ParseFolder();
+
+        // Chaque cUnit => AST d'un fichier .java
+        for(CompilationUnit cUnit : parser.getcUnits()){
+            parcours(cUnit);
+        }
+        return true;
+    }
+
+
+    // Renvoie la liste des appels de méthodes présent dans une méthode
+    public List<MethodInvocation> getMethodInvocation(MethodDeclaration methode){
+
+        MethodInvocationVisitor  visitor = new MethodInvocationVisitor();
+
+        return visitor.getMethodsInvocation(methode);
+
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        aretes
+                .stream()
+                .forEach(arete -> System.out.println(arete.getFirst() + " ---> " + arete.getSecond()));
+
+
+        return sb.toString();
+    }
+
+
+}
