a468e44bce6dc59c048623ca56134f2917fbee5d
Yoann <yoann.reyne@gmail.com>
2023-10-06 10:17:45 +0200

    add

--- Yoann/tp1.ast/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //pour l'instant je vais implementee le design pattern visiteur
-    }
-}
