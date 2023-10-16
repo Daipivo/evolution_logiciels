96667c54b7bc0831ed9703d01446727ad6d22e38
Yoann <yoann.reyne@gmail.com>
2023-10-16 09:03:08 +0200

    add

--- TP2/tp2/src/main/java/couplage/Couplage.java
+++ TP2/tp2/src/main/java/couplage/Couplage.java
@@ -1,16 +1,20 @@
 package couplage;
 
 import graph.CallGraph;
+import graph.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Couplage {
 
     private CallGraph graph;
     private int cpt = 0;
+
+    public Set<Pair<String, String>> getTest() {
+        return test;
+    }
+
+    private Set<Pair<String, String>> test = new HashSet<>();
 
     public Couplage(CallGraph graph){
         this.graph = graph;
@@ -34,6 +38,14 @@ public class Couplage {
             for (int j = i + 1; j < clsList.size(); j++) {
 
 //                System.out.println(clsList.get(i) + " " + clsList.get(j));
+//
+//                if(clsList.get(i).equals("CallGraph")){
+//                    System.out.println(clsList.get(j));
+//                }
+//
+//                if(clsList.get(j).equals("CallGraph")){
+//                    System.out.println(clsList.get(i));
+//                }
 
                 numberOfRelationsBtwAllClasses += getCouplage(clsList.get(i), clsList.get(j), false);
 //                System.out.println(numberOfRelationsBtwAllClasses);
@@ -51,25 +63,20 @@ public class Couplage {
         Map<String, List<String>> class1Methods = graph.getCalledMethodsInClasse(class1);
         Map<String, List<String>> class2Methods = graph.getCalledMethodsInClasse(class2);
 
-//        System.out.println(class1 + " " + class1Methods);
-//
-//        System.out.println(" ======== ");
-//
-//        System.out.println(class2 + " " + class2Methods);
-
         double numberOfRelationsBtwClasses = 0;
 
         for ( String cls1m : class1Methods.keySet()){
 
-//            System.out.println("Methode 1 ==> " + cls1m);
-
             for ( String methodscls1 : class1Methods.get(cls1m)){
 
-//                System.out.println("Methode 1 bis ==> " + methodscls1);
-
-
                 if(class2Methods.keySet().contains(methodscls1)){
-
+                    test.add(new Pair<>(class2 + ":" + cls1m, class1 + ":" + methodscls1));
+                    if(class1.equals("CallGraph")){
+                        System.out.println(class2);
+                    }
+                    if(class2.equals("CallGraph")){
+                        System.out.println(class1);
+                    }
                     numberOfRelationsBtwClasses++;
                     cpt++;
                 }
@@ -79,20 +86,18 @@ public class Couplage {
         if(!isRefl) {
             for (String cls2m : class2Methods.keySet()) {
 
-//            System.out.println("Methode 2 ==> " + cls2m);
 
                 for (String methodscls2 : class2Methods.get(cls2m)) {
 
-//                System.out.println("Methode 2 bis ==> " + methodscls2);
-                    if (class1Methods.keySet().contains(methodscls2)){
-                        if(methodscls2.equals(cls2m)){
-                            System.out.println(cls2m + " " + methodscls2);
-                        }
-                    }
-
-
                     if (class1Methods.keySet().contains(methodscls2) && !methodscls2.equals(cls2m))  {
-
+//                        System.out.println(class1 + ":" + cls2m);
+                        test.add(new Pair<>(class1 + ":" + cls2m, class2 + ":" + methodscls2));
+                        if(class1.equals("CallGraph")){
+                            System.out.println(class2);
+                        }
+                        if(class2.equals("CallGraph")){
+                            System.out.println(class1);
+                        }
                         numberOfRelationsBtwClasses++;
                         cpt++;
 
--- TP2/tp2/src/main/java/couplage/MainCouplage.java
+++ TP2/tp2/src/main/java/couplage/MainCouplage.java
@@ -2,18 +2,21 @@ package couplage;
 
 
 import graph.CallGraph;
+import graph.Pair;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainCouplage {
 
     public static void main(String[] args) throws IOException {
 
-        CallGraph graph = new CallGraph("/home/e20180003955/Bureau/evolution_logiciels/TP2/tp2");
+        CallGraph graph = new CallGraph("/home/reyne/Bureau/evolution_logiciels/TP2/tp2");
         graph.start();
 
         System.out.println(graph.getNbrAretesIntern());
-        graph.getAretesIntern().stream().forEach(p -> System.out.println(p.getFirst() + " ==> " + p.getSecond()));
+//        graph.getAretesIntern().stream().forEach(p -> System.out.println(p.getFirst() + " ==> " + p.getSecond()));
 
         Couplage couplage = new Couplage(graph);
 
@@ -21,7 +24,44 @@ public class MainCouplage {
 //
         System.out.println(couplage.getCouplageGraph());
 
-        System.out.println(couplage.getCpt());
+//        System.out.println(couplage.getCpt());
+
+        System.out.println(graph.getAretesIntern().size());
+        System.out.println(couplage.getTest().size());
+//
+//
+//        Set<Pair<String, String>> difference = new HashSet<>();
+//
+//        for(Pair<String, String> pTest : couplage.getTest()) {
+//            boolean isFound = false;
+//            for(Pair<String, String> pIntern : graph.getAretesIntern()) {
+//                if((pTest.getFirst().equals(pIntern.getFirst()) && pTest.getSecond().equals(pIntern.getSecond())) ||
+//                        (pTest.getFirst().equals(pIntern.getSecond()) && pTest.getSecond().equals(pIntern.getFirst()))) {
+//                    isFound = true;
+//                    break;
+//                }
+//            }
+//            if(!isFound) {
+//                difference.add(pTest);
+//            }
+//        }
+
+        System.out.println("================>1");
+        graph.getAretesIntern()
+                .stream().forEach(p-> System.out.println(p.getFirst() + " " + p.getSecond()));
+//
+//
+//        System.out.println("================>2");
+//        couplage.getTest()
+//                .stream().forEach(p-> System.out.println(p.getFirst() + " " + p.getSecond()));
+
+//
+//        System.out.println("================>3");
+//        difference
+//                .stream().forEach(p-> System.out.println(p.getFirst() + " " + p.getSecond()));
+//
 
     }
+
+
 }
